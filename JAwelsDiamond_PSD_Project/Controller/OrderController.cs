29629c02ac5f5b784c2873ef861ce809c2506378
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-05-27 21:51:46 +0700

    .

---
+++ JAwelsDiamond_PSD_Project/Controller/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JAwelsDiamond_PSD_Project.Controller
+{
+	public class OrderController
+	{
+	}
+}
---
+++ JAwelsDiamond_PSD_Project/Repository/OrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JAwelsDiamond_PSD_Project.Repository
+{
+	public class OrderRepository
+	{
+	}
+}
