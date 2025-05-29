465ca19aae51f7931ce939ca4f72f76b01608f78
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-05-29 17:29:12 +0700

    .

--- JAwelsDiamond_PSD_Project/Handler/OrderHandler.cs
+++ JAwelsDiamond_PSD_Project/Handler/OrderHandler.cs
@@ -1,15 +1,13 @@
 ﻿using JAwelsDiamond_PSD_Project.Models;
-using System;
+using JAwelsDiamond_PSD_Project.Repository;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace JAwelsDiamond_PSD_Project.Handler
+namespace jawelsdiamond_psd_project.handler
 {
-	public class OrderHandler
+	public class orderhandler
 	{
-		transactionrepo
-		public bool confirmPackage(int id)
+		TransactionRepository repo = new TransactionRepository();
+		public bool confirmpackage(int id)
 		{
 			TransactionHeader th = repo.getTransactionHeader(id);
 			th.TransactionStatus = "done";
@@ -17,15 +15,15 @@ namespace JAwelsDiamond_PSD_Project.Handler
 			return response;
 		}
 
-		public bool rejectPackage(int id)
+		public bool rejectpackage(int id)
 		{
-			*//*TransactionHeader th = repo.getTransactionHeader(id);
+			TransactionHeader th = repo.getTransactionHeader(id);
 			th.TransactionStatus = "rejected";
 			bool response = repo.updateTransactionHeader(th);
-			return response;*//*
+			return response;
 		}
 
-		public bool transactionExists(int id)
+		public bool transactionexists(int id)
 		{
             TransactionHeader th = repo.getTransactionHeader(id);
 			if(th == null)
@@ -35,10 +33,10 @@ namespace JAwelsDiamond_PSD_Project.Handler
 			return true;
         }
 
-		public List<TransactionHeader> getAllTransactions(int userId)
+		public List<TransactionHeader> getalltransactions(int userid)
 		{
-			List<TransactionHeader> allTh = repo.getAllTransactions(userId);
-			return allTh;
+			List<TransactionHeader> allth = repo.getAllTransactions(userid);
+			return allth;
 		}
 	}
 }
