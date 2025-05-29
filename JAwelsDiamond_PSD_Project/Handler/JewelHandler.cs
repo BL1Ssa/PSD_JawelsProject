a5079112d001f9243021d3cca4458f885c56c27c
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-05-29 16:53:01 +0700

    g

--- JAwelsDiamond_PSD_Project/Handler/JewelHandler.cs
+++ JAwelsDiamond_PSD_Project/Handler/JewelHandler.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace JAwelsDiamond_PSD_Project.Handler
-{
-	JewelRepository repo = new JewelRepository();
-	public class JewelHandler
-	{
-		
-	}
-}
+﻿
