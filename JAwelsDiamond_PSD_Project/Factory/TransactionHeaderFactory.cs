d5119542f5b2260df2e80db4325d9d862521242d
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-05-29 16:58:47 +0700

    .

---
+++ JAwelsDiamond_PSD_Project/Factory/TransactionHeaderFactory.cs
@@ -0,0 +1,38 @@
+﻿using JAwelsDiamond_PSD_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JAwelsDiamond_PSD_Project.Factory
+{
+	public class TransactionHeaderFactory
+	{
+		public TransactionHeader createTransactionHeader(int transactionId, int userId, DateTime transactionDate, string paymentMethod, string transactionStatus)
+		{
+			TransactionHeader th = new TransactionHeader();
+			th.TransactionID = transactionId;
+			th.UserID = userId;
+			th.TransactionDate = transactionDate;
+			th.PaymentMethod = paymentMethod;
+			th.TransactionStatus = transactionStatus;
+
+			return th;
+		}
+
+        public TransactionHeader createNewTransactionHeader(int userId, DateTime transactionDate, string paymentMethod, string transactionStatus)
+        {
+            int id = repo.getLastHeaderId();
+
+
+            TransactionHeader th = new TransactionHeader();
+            th.TransactionID = id;
+            th.UserID = userId;
+            th.TransactionDate = transactionDate;
+            th.PaymentMethod = paymentMethod;
+            th.TransactionStatus = transactionStatus;
+
+            return th;
+        }
+    }
+}
